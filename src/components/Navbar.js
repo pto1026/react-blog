0d499867f2e56c4bdcf627cb47743f47ff8f6ff4
Patrick Sparks <pto1026@gmail.com>
2022-02-27 14:03:36 -0700

    added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div>
-
+            <nav className="navbar navbar-light bg-light">
+                <div className="container-fluid">
+                    <Link to='/'>
+                    <a className="navbar-brand">Squad Blog</a>
+                    </Link>
+                </div>
+            </nav>
         </div>
     )
 }
